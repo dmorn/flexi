133cfa257866e0d33682c01b00ee5094486fd37e
Daniel Morandini <danielmorandini@me.com>
2020-07-05 14:56:07 +0200

    Add json process helper

---
+++ json/helper.go
@@ -0,0 +1,29 @@
+package json
+
+import (
+	"encoding/json"
+	"io"
+
+	"github.com/jecoz/flexi"
+)
+
+type ProcessHelper struct {
+	*flexi.Stdio
+}
+
+func encode(w io.WriteCloser, v interface{}) error {
+	defer w.Close()
+	return json.NewEncoder(w).Encode(v)
+}
+
+func (h *ProcessHelper) Retv(v interface{}) error {
+	return encode(h.Stdio.Retv(), v)
+}
+
+func (h *ProcessHelper) Err(err error) error {
+	return encode(h.Stdio.Err(), &struct{
+		Error string `json:"error"`
+	}{
+		Error: err.Error(),
+	})
+}
