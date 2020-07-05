4324986b87101fee45aac55db36b82102ff2caba
Daniel Morandini <danielmorandini@me.com>
2020-07-05 14:55:40 +0200

    Add updated process/processor definition

---
+++ proc.go
@@ -0,0 +1,17 @@
+package flexi
+
+import "io"
+
+type Stdio struct {
+	In io.Reader
+	Err func() io.WriteCloser
+	Retv func() io.WriteCloser
+}
+
+type Processor interface {
+	Run(*Stdio)
+}
+
+type ProcessorFunc func(*Stdio)
+func (f ProcessorFunc) Run(i *Stdio) { f(i) }
+
