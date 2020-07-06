7fea247a68d71d4434e0983ed77e7d3b4132627f
Daniel Morandini <danielmorandini@me.com>
2020-07-06 09:28:19 +0200

    Format

--- examples/echo64/main.go
+++ examples/echo64/main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"bytes"
+	"encoding/base64"
 	"flag"
 	"io"
 	"log"
 	"net"
 	"os"
 	"os/signal"
-	"encoding/base64"
 
 	"github.com/jecoz/flexi"
 	"github.com/jecoz/flexi/json"
@@ -42,12 +42,12 @@ func main() {
 		encoded := base64.StdEncoding.EncodeToString(b.Bytes())
 
 		h := &json.ProcessHelper{i}
-		if err := h.Retv(&struct{
+		if err := h.Retv(&struct {
 			Original string `json:"original"`
-			Base64 string `json:"base64"`
+			Base64   string `json:"base64"`
 		}{
 			Original: b.String(),
-			Base64: encoded,
+			Base64:   encoded,
 		}); err != nil {
 			panic("retv: " + err.Error())
 		}
