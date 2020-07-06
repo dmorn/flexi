7592079975a2773d025eb1f9d562ecb61ffb8a70
Daniel Morandini <danielmorandini@me.com>
2020-07-06 18:40:15 +0200

    Remove Fargate public fields

--- fargate/fargate.go
+++ fargate/fargate.go
@@ -15,10 +15,6 @@ import (
 const MaxBackoffMsec = 30000
 
 type Fargate struct {
-	Cluster        string
-	Subnets        []string
-	SecurityGroups []string
-
 	sess   *session.Session
 	client *ecs.ECS
 }
@@ -37,9 +33,9 @@ func (f *Fargate) lazyClient() *ecs.ECS {
 	return f.client
 }
 
-func (f *Fargate) DescribeTask(ctx context.Context, arn string) (*ecs.Task, error) {
+func (f *Fargate) DescribeTask(ctx context.Context, cluster, arn string) (*ecs.Task, error) {
 	input := &ecs.DescribeTasksInput{
-		Cluster: stringPtr(f.Cluster),
+		Cluster: stringPtr(cluster),
 		Tasks:   stringPtrSlice([]string{arn}),
 	}
 	if err := input.Validate(); err != nil {
@@ -61,7 +57,7 @@ func (f *Fargate) DescribeTask(ctx context.Context, arn string) (*ecs.Task, erro
 	return resp.Tasks[0], nil
 }
 
-func (f *Fargate) waitRunningTask(ctx context.Context, arn string) (task *ecs.Task, err error) {
+func (f *Fargate) waitRunningTask(ctx context.Context, cluster, arn string) (task *ecs.Task, err error) {
 	// Stop when the context is invalidated or the response is no longer
 	// successfull. We're waiting a pending process to become running here,
 	// not to resume from a lost connection.
@@ -76,7 +72,7 @@ func (f *Fargate) waitRunningTask(ctx context.Context, arn string) (task *ecs.Ta
 		timer := time.NewTimer(wait)
 		select {
 		case <-timer.C:
-			task, err = f.DescribeTask(ctx, arn)
+			task, err = f.DescribeTask(ctx, cluster, arn)
 			if err != nil {
 				return
 			}
@@ -95,16 +91,23 @@ func (f *Fargate) waitRunningTask(ctx context.Context, arn string) (task *ecs.Ta
 	}
 }
 
-func (f *Fargate) RunTask(ctx context.Context, def string) (*ecs.Task, error) {
+type runTaskInput struct {
+	Cluster        string
+	TaskDefinition string
+	Subnets        []string
+	SecurityGroups []string
+}
+
+func (f *Fargate) RunTask(ctx context.Context, p runTaskInput) (*ecs.Task, error) {
 	input := &ecs.RunTaskInput{
-		Cluster:        stringPtr(f.Cluster),
+		Cluster:        stringPtr(p.Cluster),
 		LaunchType:     stringPtr(ecs.LaunchTypeFargate),
-		TaskDefinition: stringPtr(def),
+		TaskDefinition: stringPtr(p.TaskDefinition),
 		NetworkConfiguration: &ecs.NetworkConfiguration{
 			AwsvpcConfiguration: &ecs.AwsVpcConfiguration{
 				AssignPublicIp: stringPtr(ecs.AssignPublicIpEnabled),
-				Subnets:        stringPtrSlice(f.Subnets),
-				SecurityGroups: stringPtrSlice(f.SecurityGroups),
+				Subnets:        stringPtrSlice(p.Subnets),
+				SecurityGroups: stringPtrSlice(p.SecurityGroups),
 			},
 		},
 	}
@@ -125,11 +128,10 @@ func (f *Fargate) RunTask(ctx context.Context, def string) (*ecs.Task, error) {
 	return resp.Tasks[0], nil
 }
 
-func (f *Fargate) StopTask(ctx context.Context, arn, reason string) error {
+func (f *Fargate) StopTask(ctx context.Context, cluster, arn string) error {
 	input := &ecs.StopTaskInput{
-		Cluster: stringPtr(f.Cluster),
+		Cluster: stringPtr(cluster),
 		Task:    stringPtr(arn),
-		Reason:  stringPtr(reason),
 	}
 	if err := input.Validate(); err != nil {
 		return err
@@ -184,22 +186,20 @@ func eniFromTask(task *ecs.Task) (string, error) {
 	return eni, nil
 }
 
-type remoteProcess struct {
-	addr    string
-	taskArn string
-}
-
-func (p *remoteProcess) Addr() string { return p.addr }
-func (p *remoteProcess) Name() string { return p.taskArn }
-
-func (f *Fargate) Spawn(ctx context.Context, t flexi.Task) (flexi.RemoteProcess, error) {
-	task, err := f.RunTask(ctx, t.Image.Name)
+func (f *Fargate) Spawn(ctx context.Context, t flexi.Task) (*flexi.RemoteProcess, error) {
+	task, err := f.RunTask(ctx, runTaskInput{
+		Cluster:        t.Image.Cluster,
+		TaskDefinition: t.Image.Name,
+		Subnets:        t.Image.Subnets,
+		SecurityGroups: t.Image.SecurityGroups,
+	})
 	if err != nil {
 		return nil, err
 	}
-	if task, err = f.waitRunningTask(ctx, *task.TaskArn); err != nil {
+	if task, err = f.waitRunningTask(ctx, t.Image.Cluster, *task.TaskArn); err != nil {
 		return nil, err
 	}
+
 	eni, err := eniFromTask(task)
 	if err != nil {
 		return nil, err
@@ -208,14 +208,17 @@ func (f *Fargate) Spawn(ctx context.Context, t flexi.Task) (flexi.RemoteProcess,
 	if err != nil {
 		return nil, err
 	}
-	return &remoteProcess{
-		addr:    net.JoinHostPort(*ifi.Association.PublicIp, t.Image.Service),
-		taskArn: *task.TaskArn,
+
+	return &flexi.RemoteProcess{
+		Addr:    net.JoinHostPort(*ifi.Association.PublicIp, t.Image.Service),
+		Name:    *task.TaskArn,
+		Cluster: t.Image.Cluster,
+		Tags:    []string{"fargate", "9p"},
 	}, nil
 }
 
-func (f *Fargate) Kill(ctx context.Context, p flexi.RemoteProcess) error {
-	return f.StopTask(ctx, p.Name(), "")
+func (f *Fargate) Kill(ctx context.Context, p *flexi.RemoteProcess) error {
+	return f.StopTask(ctx, p.Cluster, p.Name)
 }
 
 func stringPtrSlice(s []string) []*string {
