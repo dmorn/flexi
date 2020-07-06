4ade416122c36c02c4b46ce4f14775489430c6e2
Daniel Morandini <danielmorandini@me.com>
2020-07-06 18:40:30 +0200

    Add echo64spawner implementation

---
+++ examples/echo64spawner/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/jecoz/flexi"
+	"github.com/jecoz/flexi/fargate"
+)
+
+func exitf(format string, args ...interface{}) {
+	fmt.Printf("error * "+format, args...)
+	os.Exit(1)
+}
+
+func main() {
+	var task flexi.Task
+	if err := json.NewDecoder(os.Stdin).Decode(&task); err != nil {
+		exitf("decode input task: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*3)
+	defer cancel()
+
+	rp, err := new(fargate.Fargate).Spawn(ctx, task)
+	if err != nil {
+		exitf("spawn task: %v", err)
+	}
+
+	if err = json.NewEncoder(os.Stdout).Encode(rp); err != nil {
+		exitf("output encode: %v", err)
+	}
+}
