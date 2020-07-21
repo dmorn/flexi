8a2e46f64992d31e9376770879c6f2470245e483
Daniel Morandini <danielmorandini@me.com>
2020-07-21 12:05:59 +0200

    Update struct visibility

--- styx/pluffer.go
+++ styx/pluffer.go
@@ -10,6 +10,8 @@ import (
 	"time"
 )
 
+// Pluffer acts like a plumbing buffer (in plan9 terms). Upon close, the
+// Pluffer calls OnClose function letting callers act on the stored bytes.
 type Pluffer struct {
 	Name    string
 	Perm    os.FileMode
@@ -19,8 +21,8 @@ type Pluffer struct {
 	buf bytes.Buffer
 }
 
-func (p *Pluffer) Stat() (os.FileInfo, error) { return PlufferInfo{p}, nil }
-func (p *Pluffer) Open() (interface{}, error) { return &Plufferio{p}, nil }
+func (p *Pluffer) Stat() (os.FileInfo, error) { return plufferInfo{p}, nil }
+func (p *Pluffer) Open() (interface{}, error) { return &plufferio{p}, nil }
 func (p *Pluffer) Truncate(size int64) error {
 	p.buf.Truncate(int(size))
 	return nil
@@ -29,27 +31,27 @@ func (p *Pluffer) Truncate(size int64) error {
 func (p *Pluffer) Len() int64                  { return int64(p.buf.Len()) }
 func (pf *Pluffer) Read(p []byte) (int, error) { return pf.buf.Read(p) }
 
-type PlufferInfo struct {
+type plufferInfo struct {
 	*Pluffer
 }
 
-func (p PlufferInfo) Name() string       { return p.Pluffer.Name }
-func (p PlufferInfo) Size() int64        { return int64(p.Pluffer.buf.Len()) }
-func (p PlufferInfo) Mode() os.FileMode  { return p.Pluffer.Perm }
-func (p PlufferInfo) ModTime() time.Time { return p.Pluffer.ModTime }
-func (p PlufferInfo) IsDir() bool        { return false }
-func (p PlufferInfo) Sys() interface{} {
+func (p plufferInfo) Name() string       { return p.Pluffer.Name }
+func (p plufferInfo) Size() int64        { return int64(p.Pluffer.buf.Len()) }
+func (p plufferInfo) Mode() os.FileMode  { return p.Pluffer.Perm }
+func (p plufferInfo) ModTime() time.Time { return p.Pluffer.ModTime }
+func (p plufferInfo) IsDir() bool        { return false }
+func (p plufferInfo) Sys() interface{} {
 	rwc, _ := p.Pluffer.Open()
 	return rwc
 }
 
-type Plufferio struct {
+type plufferio struct {
 	pluffer *Pluffer
 }
 
-func (pf *Plufferio) Read(p []byte) (int, error)  { return pf.pluffer.buf.Read(p) }
-func (pf *Plufferio) Write(p []byte) (int, error) { return pf.pluffer.buf.Write(p) }
-func (pf *Plufferio) Close() error {
+func (pf *plufferio) Read(p []byte) (int, error)  { return pf.pluffer.buf.Read(p) }
+func (pf *plufferio) Write(p []byte) (int, error) { return pf.pluffer.buf.Write(p) }
+func (pf *plufferio) Close() error {
 	if onClose := pf.pluffer.OnClose; onClose != nil {
 		return onClose(pf.pluffer)
 	}
