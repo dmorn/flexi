4c6ede32b76698e4b696db4bcbde3e8ec14e400e
Daniel Morandini <danielmorandini@me.com>
2020-07-28 15:55:50 +0200

    Add fs package
    Introduce a new abstraction that will help handling
    the bridge between styx and our files.

---
+++ fs/fs.go
@@ -0,0 +1,33 @@
+package fs
+
+import (
+	"io"
+	"os"
+)
+
+// File describes the minimal list of functions
+// required to interact with a file in flexi's
+// context.
+// Some files might be Directory instances too.
+type File interface {
+	// Open File for i/o operations.
+	Open() (io.ReadWriteCloser, error)
+	// Stat returns file information. Use
+	// it to determine wether the File is
+	// actually a Directory or not.
+	Stat() (os.FileInfo, error)
+}
+
+type Directory interface {
+	Readdir(n int) ([]os.FileInfo, error)
+}
+
+// RoFS is a read-only file-system.
+type RoFS interface {
+	Open(path string) (File, error)
+}
+
+type FS interface {
+	RoFS
+	Create(path string, newfile File) error
+}
