2b81b92bbb3589c1e071627881d2024b43f53f91
Daniel Morandini <danielmorandini@me.com>
2020-07-28 15:58:31 +0200

    Cleanup styx leaving only some helper functions

--- styx/srv.go
+++ styx/srv.go
@@ -5,28 +5,61 @@
 package styx
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"io"
 	"log"
 	"net"
-	"os"
-	"os/exec"
-	"path/filepath"
-	"time"
 
 	"aqwari.net/net/styx"
-	"github.com/jecoz/flexi"
+	"github.com/jecoz/flexi/file"
+	"github.com/jecoz/flexi/fs"
 )
 
 type Srv struct {
-	Mtpt string
-	Ln   net.Listener
-	S    flexi.Spawner
+	Ln net.Listener
+	FS fs.FS
+}
 
-	index int
-	fs    *fs
+func (srv *Srv) handleRequest(t styx.Request) {
+	switch msg := t.(type) {
+	case styx.Ttruncate:
+		// TODO: implement if needed
+		msg.Rtruncate(nil)
+	case styx.Tutimes:
+		// TODO: implement if needed
+		msg.Rutimes(nil)
+	case styx.Tcreate:
+		b := file.NewBucket(msg.Name, msg.Mode, 2048)
+		if err := srv.FS.Create(msg.Path(), b); err != nil {
+			msg.Rerror(err.Error())
+			return
+		}
+		msg.Rcreate(b.Open())
+	case styx.Topen, styx.Twalk, styx.Tstat:
+		// All these messages require an open first.
+		// We're taking care of it in a single place.
+	default:
+		// Handled with default responses.
+		return
+	}
+
+	file, err := srv.FS.Open(t.Path())
+	if err != nil {
+		t.Rerror(err.Error())
+		return
+	}
+	switch msg := t.(type) {
+	case styx.Topen:
+		msg.Ropen(file.Open())
+	case styx.Twalk:
+		msg.Rwalk(file.Stat())
+	case styx.Tstat:
+		msg.Rstat(file.Stat())
+	}
+}
+
+func (srv *Srv) Serve9P(s *styx.Session) {
+	for s.Next() {
+		srv.handleRequest(s.Request())
+	}
 }
 
 func (s *Srv) Serve() error {
@@ -36,72 +69,12 @@ func (s *Srv) Serve() error {
 		}
 	})
 	srv := &styx.Server{
-		Handler:  styx.Stack(echo, s),
-		ErrorLog: log.New(os.Stderr, "", log.LstdFlags),
+		Handler: styx.Stack(echo, s),
 	}
 	return srv.Serve(s.Ln)
 }
 
-func (srv *Srv) Serve9P(s *styx.Session) {
-	for s.Next() {
-		srv.fs.serveRequest(s.Request())
-	}
-}
-
-func (srv *Srv) spawn(ctx context.Context, rc io.ReadCloser) error {
-	defer rc.Close()
-
-	var task flexi.Task
-	if err := json.NewDecoder(rc).Decode(&task); err != nil {
-		return fmt.Errorf("spawn process: %w", err)
-	}
-	rp, err := srv.S.Spawn(ctx, task)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("SPAWNED")
-	if err := json.NewEncoder(os.Stdout).Encode(rp); err != nil {
-		return err
-	}
-	name := fmt.Sprintf("%d", srv.index)
-	mtpt := filepath.Join(srv.Mtpt, name)
-	fmt.Printf("name: %v, mptp: %v\n", name, mtpt)
-	cmd := exec.Command("9", "mount", rp.Addr, mtpt)
-	if err := cmd.Run(); err != nil {
-		return err
-	}
-	srv.index++
-
-	fmt.Printf("MOUNTED @ %v\n", mtpt)
-	return nil
-}
-
-func (srv *Srv) SpawnProcess(rc io.ReadCloser) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*5)
-	defer cancel()
-	if err := srv.spawn(ctx, rc); err != nil {
-		log.Printf("ERROR * %v", err)
-		return err
-	}
-	return nil
-}
-
-// ServeFlexi creates a styx process serving 9p over ln. To gracefully shutdown
-// the server, close the listener.
-func ServeFlexi(ln net.Listener, mntpt string, s flexi.Spawner) error {
-	srv := &Srv{Mtpt: mntpt, Ln: ln, S: s}
-	ctl := NewInputBuffer("ctl", srv.SpawnProcess)
-	root := &Dir{
-		Name:    "",
-		Perm:    os.ModePerm,
-		ModTime: time.Now(),
-		Ls: func() []File {
-			union := append([]File{ctl}, DiskLs(mntpt)()...)
-			return union
-		},
-	}
-	srv.fs = &fs{Root: root}
-
+func Serve(ln net.Listener, fs fs.FS) error {
+	srv := &Srv{Ln: ln, FS: fs}
 	return srv.Serve()
 }
