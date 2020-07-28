b1423058182fdd63c93fb34a291809c44e0b4ed3
Daniel Morandini <danielmorandini@me.com>
2020-07-28 15:58:50 +0200

    Add Process implementation

--- process.go
+++ process.go
@@ -4,7 +4,16 @@
 
 package flexi
 
-import "io"
+import (
+	"bytes"
+	"io"
+	"net"
+
+	"github.com/jecoz/flexi/file"
+	"github.com/jecoz/flexi/file/memfs"
+	"github.com/jecoz/flexi/fs"
+	"github.com/jecoz/flexi/styx"
+)
 
 type Stdio struct {
 	// In contains the input bytes. For 9p processes,
@@ -30,3 +39,47 @@ type Processor interface {
 type ProcessorFunc func(*Stdio)
 
 func (f ProcessorFunc) Run(i *Stdio) { f(i) }
+
+type Process struct {
+	FS     fs.FS
+	Ln     net.Listener
+	Runner Processor
+}
+
+func (p *Process) Serve() error {
+	return styx.Serve(p.Ln, p.FS)
+}
+
+func ServeProcess(ln net.Listener, r Processor) error {
+	err := file.NewMulti("err")
+	retv := file.NewMulti("retv")
+	ctl := file.NewPlumber("ctl", func(p *file.Plumber) bool {
+		if p.Size() == 0 {
+			return false
+		}
+		buf := new(bytes.Buffer)
+		if _, err := io.Copy(buf, p); err != nil {
+			return false
+		}
+
+		go func() {
+			stdio := &Stdio{
+				In:   buf,
+				Err:  err,
+				Retv: retv,
+			}
+			r.Run(stdio)
+			err.Close()
+			retv.Close()
+		}()
+		return true
+	})
+
+	root := file.NewDirFiles("", ctl, err, retv)
+	p := Process{
+		FS:     memfs.New(root),
+		Ln:     ln,
+		Runner: r,
+	}
+	return p.Serve()
+}
