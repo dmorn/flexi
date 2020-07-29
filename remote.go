f07e65b5a2d76b507a431d03fabdc4e02cd92d12
Daniel Morandini <danielmorandini@me.com>
2020-07-29 17:19:41 +0200

    Add mount function

--- remote.go
+++ remote.go
@@ -19,6 +19,68 @@ type Remote struct {
 	mtpt string
 }
 
+func (r *Remote) mount(ctx context.Context, path string, stdio *Stdio) {
+	h := &JsonHelper{}
+	herr := func(err error) {
+		h.Err(stdio.Err, err)
+	}
+	status := func(format string, args ...interface{}) {
+		fmt.Fprintf(stdio.Status, format+"\n", args...)
+	}
+
+	task, err := DecodeTask(stdio.In)
+	if err != nil {
+		herr(err)
+		return
+	}
+	status("task %v decoded", task.ID)
+
+	rp, err := r.Spawn(ctx, task)
+	if err != nil {
+		herr(err)
+		return
+	}
+	status("remote process %v spawned @ %v", rp.Name, rp.Addr)
+
+	// From now on we also need to remove the spawned
+	// process in case of error to avoid resource leaks.
+	oldherr := herr
+	herr = func(err error) {
+		r.Kill(ctx, rp)
+		oldherr(err)
+		return
+	}
+
+	// TODO: OS dependent!
+	cmd := exec.CommandContext(ctx, "9", "mount", rp.Addr, path)
+	if err := cmd.Run(); err != nil {
+		herr(err)
+		return
+	}
+	status("remote process %v mounted @ %v", rp.Name, path)
+
+	oldherr = herr
+	herr = func(err error) {
+		exec.CommandContext(ctx, "umount", path).Run()
+		os.RemoveAll(path)
+		oldherr(err)
+		return
+	}
+
+	spawned, err := os.Create(filepath.Join(path, "spawned"))
+	if err != nil {
+		herr(err)
+		return
+	}
+	defer spawned.Close()
+
+	if err = EncodeRemoteProcess(spawned, rp); err != nil {
+		herr(err)
+		return
+	}
+	status("remote process info encoded & saved")
+}
+
 func NewRemote(mtpt, name string, s Spawner) (*Remote, error) {
 	// First check that the file is not present already.
 	// In that case, it means this remote should've been
@@ -29,61 +91,26 @@ func NewRemote(mtpt, name string, s Spawner) (*Remote, error) {
 		return nil, fmt.Errorf("remote exists already at %v", path)
 	}
 
+	r := &Remote{mtpt: mtpt, Spawner: s}
 	errfile := file.NewMulti("err")
-	h := &JsonHelper{}
-	herr := func(err error) bool {
-		h.Err(errfile, err)
-		return false
-	}
+	statusfile := file.NewMulti("status")
 
 	spawn := file.NewPlumber("spawn", func(p *file.Plumber) bool {
-		defer errfile.Close()
+		go func() {
+			defer errfile.Close()
+			defer statusfile.Close()
 
-		task, err := DecodeTask(p)
-		if err != nil {
-			return herr(err)
-		}
-		ctx := context.Background()
-		rp, err := s.Spawn(ctx, *task)
-		if err != nil {
-			return herr(err)
-		}
-
-		// From now on we also need to remove the spawned
-		// process in case of error to avoid resource leaks.
-		herr = func(err error) bool {
-			s.Kill(ctx, rp)
-			return herr(err)
-		}
-
-		// TODO: OS dependent!
-		cmd := exec.CommandContext(ctx, "9", "mount", rp.Addr, path)
-		if err := cmd.Run(); err != nil {
-			return herr(err)
-		}
-		herr = func(err error) bool {
-			exec.CommandContext(ctx, "umount", path).Run()
-			os.RemoveAll(path)
-			return herr(err)
-		}
-
-		spawned, err := os.Create(filepath.Join(path, "spawned"))
-		if err != nil {
-			return herr(err)
-		}
-		defer spawned.Close()
-
-		if err = EncodeRemoteProcess(spawned, rp); err != nil {
-			return herr(err)
-		}
+			r.mount(context.Background(), path, &Stdio{
+				In:     p,
+				Err:    errfile,
+				Status: statusfile,
+			})
+		}()
 		return true
 	})
-
-	return &Remote{
-		mtpt:    mtpt,
-		Spawner: s,
-		Dir: file.NewDirLS(name, func() []fs.File {
-			return append([]fs.File{spawn, errfile}, file.DiskLS(path)()...)
-		}),
-	}, nil
+	static := []fs.File{spawn, errfile, statusfile}
+	r.Dir = file.NewDirLS(name, func() []fs.File {
+		return append(static, file.DiskLS(path)()...)
+	})
+	return r, nil
 }
