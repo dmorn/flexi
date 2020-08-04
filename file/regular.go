df6b4f764138b17945806c113df2b5f9d2574dd4
Daniel Morandini <danielmorandini@me.com>
2020-08-04 12:13:23 +0200

    Use os.Create instead of os.Open to write files

--- file/regular.go
+++ file/regular.go
@@ -13,7 +13,7 @@ type Regular struct {
 	path string
 }
 
-func (r *Regular) Open() (io.ReadWriteCloser, error) { return os.Open(r.path) }
+func (r *Regular) Open() (io.ReadWriteCloser, error) { return os.Create(r.path) }
 func (r *Regular) Stat() (os.FileInfo, error)        { return os.Stat(r.path) }
 func (r *Regular) Close() error                      { return nil }
 
