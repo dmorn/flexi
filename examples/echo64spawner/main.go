47315c73b0e19784092daf6eb303ec35861c6a5e
Daniel Morandini <danielmorandini@me.com>
2020-08-05 09:42:00 +0200

    Adapt to new Spawner interface

--- examples/echo64spawner/main.go
+++ examples/echo64spawner/main.go
@@ -23,11 +23,11 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*3)
 	defer cancel()
 
-	_, p, err := new(fargate.Fargate).Spawn(ctx, os.Stdin)
+	rp, err := new(fargate.Fargate).Spawn(ctx, os.Stdin)
 	if err != nil {
 		exitf("spawn task: %v", err)
 	}
-	if _, err := io.Copy(os.Stdout, p); err != nil {
+	if _, err := io.Copy(os.Stdout, rp.Spawned); err != nil {
 		exitf("output copy: %v", err)
 	}
 }
