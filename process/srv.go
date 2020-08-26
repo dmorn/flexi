3343529bc588f52a5045b27651b83b186125e29e
Daniel Morandini <danielmorandini@me.com>
2020-08-26 17:51:51 +0200

    Move process in its own package. Use new fs features

--- process/srv.go
+++ process/srv.go
@@ -2,10 +2,9 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
-package flexi
+package process
 
 import (
-	"bytes"
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
@@ -13,11 +12,12 @@ import (
 	"log"
 	"net"
 	"strconv"
+	"os"
 
-	"github.com/jecoz/flexi/file"
-	"github.com/jecoz/flexi/file/memfs"
 	"github.com/jecoz/flexi/fs"
-	"github.com/jecoz/flexi/styx"
+	"github.com/jecoz/flexi/fs/synthfs"
+	"github.com/jecoz/flexi/fs/styxfs"
+	"aqwari.net/net/styx"
 )
 
 type Stdio struct {
@@ -33,85 +33,70 @@ type Stdio struct {
 	State io.WriteCloser
 }
 
-// Processor describes an entity that is capable of executing
+// Runner describes an entity that is capable of executing
 // a task reading and writing from Stdio.
-type Processor interface {
+type Runner interface {
 	// Stdio Err and Retv buffers should not be used
 	// after Run returns.
 	Run(*Stdio)
 }
 
-// ProcessorFunc is an helper Processor implementation that
-// allows to make Processor even ordinary functions.
-type ProcessorFunc func(*Stdio)
+// RunnerFunc is an helper Runner implementation that
+// allows to make Runner even ordinary functions.
+type RunnerFunc func(*Stdio)
 
-func (f ProcessorFunc) Run(i *Stdio) { f(i) }
+func (f RunnerFunc) Run(i *Stdio) { f(i) }
 
-type Process struct {
-	FS     fs.FS
-	Ln     net.Listener
-	Runner Processor
+type Srv struct {
+	*styxfs.FS
 }
 
-func (p *Process) Serve() error {
-	return styx.Serve(p.Ln, p.FS)
-}
-
-func ServeProcess(ln net.Listener, r Processor) error {
-	err := file.NewMulti("err")
-	retv := file.NewMulti("retv")
-	state := file.NewMulti("state")
-	in := file.NewPlumber("in", func(p *file.Plumber) bool {
-		buf := new(bytes.Buffer)
-		if _, err := io.Copy(buf, p); err != nil {
-			return false
-		}
-
-		go func() {
-			stdio := &Stdio{
-				In:    buf,
-				Err:   err,
-				Retv:  retv,
-				State: state,
-			}
-			r.Run(stdio)
-			err.Close()
-			retv.Close()
-		}()
-		return true
-	})
-	root := file.NewDirFiles(
-		"",
-		in,
-		err,
-		retv,
-		state,
-	)
-	p := Process{
-		FS:     memfs.New(root),
-		Ln:     ln,
-		Runner: r,
+func (s *Srv) Serve(ln net.Listener) error {
+	srv := &styx.Server{
+		// Remember that it is possible to stack handlers
+		// using the styx.Stack helper.
+		Handler: s,
+		TraceLog: log.New(os.Stderr, "", log.Ltime),
 	}
-	log.Printf("*** listening on %v", ln.Addr())
-	return p.Serve()
+	return srv.Serve(ln)
 }
 
-// Use NewProcessHelper to create a working instance
-// of ProcessHelper.
-type ProcessHelper struct {
+func NewSrv(fsys fs.RWFS) *Srv { return &Srv{styxfs.New(fsys)} }
+
+func addBuffer(fsys *synthfs.FS, n string, m os.FileMode, in ...string) *synthfs.Buffer {
+	b := &synthfs.Buffer{
+		Name: n,
+		Mode: m,
+	}
+	if err := fsys.AddOpener(b, n, in...); err != nil {
+		panic(err)
+	}
+	return b
+}
+
+func Serve(ln net.Listener, r Runner) error {
+	fsys := new(synthfs.FS)
+	addBuffer(fsys, "hello", os.ModePerm)
+
+	log.Printf("*** listening on %v", ln.Addr())
+	return NewSrv(fsys).Serve(ln)
+}
+
+// Use NewHelper to create a working instance of Helper.
+type Helper struct {
 	tot float64
 	i   *Stdio
 	pw  *csv.Writer
 }
 
-func (h *ProcessHelper) relayErr(err error) {
+func (h *Helper) relayErr(err error) {
 	// TODO: panic-ing here is just a tmp solution.
 	// I would rather prefer to contact a human.
 	// Log? Email? Slack?
 	panic(err)
 }
 
-func (h *ProcessHelper) Progress(step int, format string, args ...interface{}) {
+func (h *Helper) Progress(step int, format string, args ...interface{}) {
 	if err := h.pw.Write([]string{
 		strconv.FormatFloat(float64(step)/h.tot, 'f', -1, 64),
 		fmt.Sprintf(format, args...),
@@ -125,7 +110,7 @@ func (h *ProcessHelper) Progress(step int, format string, args ...interface{}) {
 	}
 }
 
-func (h *ProcessHelper) Err(err error) {
+func (h *Helper) Err(err error) {
 	// TODO: if we write multiple times to h.Err we'll produce
 	// and invalid json payload. We should be able to truncate
 	// the file instead.
@@ -138,27 +123,27 @@ func (h *ProcessHelper) Err(err error) {
 	}
 }
 
-func (h *ProcessHelper) Errf(format string, args ...interface{}) {
+func (h *Helper) Errf(format string, args ...interface{}) {
 	h.Err(fmt.Errorf(format, args...))
 }
 
-func (h *ProcessHelper) Retv(v interface{}) {
+func (h *Helper) Retv(v interface{}) {
 	if err := json.NewEncoder(h.i.Retv).Encode(v); err != nil {
 		// Try telling the user about the error!
 		h.Err(err)
 	}
 }
 
-func (h *ProcessHelper) JSONDecodeInput(v interface{}) error {
+func (h *Helper) JSONDecodeInput(v interface{}) error {
 	return json.NewDecoder(h.i.In).Decode(v)
 }
 
 // Done writes the final "Done" message, indicating that the process
 // finished doing its task and will not post any more status update.
-func (h *ProcessHelper) Done() { h.Progress(int(h.tot), "done!") }
+func (h *Helper) Done() { h.Progress(int(h.tot), "done!") }
 
-func NewProcessHelper(i *Stdio, tot int) *ProcessHelper {
-	return &ProcessHelper{
+func NewHelper(i *Stdio, tot int) *Helper {
+	return &Helper{
 		tot: float64(tot),
 		i:   i,
 		pw:  csv.NewWriter(i.State),
